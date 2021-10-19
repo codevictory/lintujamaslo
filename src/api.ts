39ea7c3acdd89d2f8a3c3aa6be1d7ef7f3e0ba02
codevictory <main@codevictory.net>
2021-10-19 22:08:11 +0300

    Add firebase api config.

---
+++ src/api.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcE9X0ueyczUqpjm0bK9UDrzvDmB96pr4",
+  authDomain: "lintujamaslo.firebaseapp.com",
+  projectId: "lintujamaslo",
+  storageBucket: "lintujamaslo.appspot.com",
+  messagingSenderId: "110340750803",
+  appId: "1:110340750803:web:89f5187720e77fc2788326"
+};
+
+const app = initializeApp(firebaseConfig);
