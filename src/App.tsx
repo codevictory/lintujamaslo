86a8cab3f70d4a63c0e1a6e6a1235a6f3b9b23d7
codevictory <main@codevictory.net>
2021-10-20 20:11:02 +0300

    Initial clean up and placeholder site.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Lintu ja Maslo</h1>
+        <p><i>Elokuussa 2022</i></p>
       </header>
     </div>
   );
