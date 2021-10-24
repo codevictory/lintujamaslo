ed5c5847d6fb3fa9af217b2debca056a243fb1ee
codevictory <main@codevictory.net>
2021-10-24 20:44:05 +0300

    React router setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
-import { Header } from "./components/header"
+import { Header } from "./components/header";
+import { Registration } from './components/registration';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div className="App">
+            <Header />
+          </div>
+        </Route>
+        <Route path="/registration/:id" component={Registration} />
+      </Switch>
+    </Router>
   );
 }
-
-export default App;
