2dd5ab07b00f9b6332ea2860649c153ee6ed4a7f
codevictory <main@codevictory.net>
2021-10-24 20:03:53 +0300

    Initial data model.

---
+++ src/model/invitation.ts
@@ -0,0 +1,4 @@
+export type Invitation = {
+    opened: boolean;
+    submitted: boolean;
+}
---
+++ src/model/visitor.ts
@@ -0,0 +1,10 @@
+export type Visitor = {
+    name: string;
+    lactoseFree: boolean;
+    dairyFree: boolean;
+    wheatFree: boolean;
+    vegan: boolean;
+    vegatarian: boolean;
+    accomodation: boolean;
+    ride: boolean;
+}
