f47a3daa0387ea19d3b2f88e66c1618c3b86ee3c
codevictory <main@codevictory.net>
2021-11-02 23:30:53 +0200

    Api function to add visitor.

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,7 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, setDoc, doc } from "firebase/firestore"; 
+import { Visitor } from './model/visitor';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBcE9X0ueyczUqpjm0bK9UDrzvDmB96pr4",
@@ -9,4 +12,15 @@ const firebaseConfig = {
   appId: "1:110340750803:web:89f5187720e77fc2788326"
 };
 
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
+const db = getFirestore();
+
+export const addVisitor = async (visitor: Visitor) => {
+  await setDoc(doc(db, "visitors", visitor.name), {
+    invitationId: visitor.invitationId,
+    allergies: visitor.allergies,
+    services: visitor.services,
+    preferences: visitor.preferences
+  });
+}
